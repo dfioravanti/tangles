f32b7618b3729aedb39203b9e0d04105a556c474
Diego Fioravanti <fioravanti.diego@gmail.com>
2020-02-28 09:35:11 +0100

    new version of the make_submodular

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,33 +1,58 @@
 import numpy as np
-from bitarray import bitarray
 
 
 def make_submodular(cuts):
 
-    # TODO: ndarray is not hashable. Find a way around it
+    """
+    Given a set of cuts we make it submodular. A set S is submodular if A union B and A intersection B are in S
+    r all A, B in S.  We achieve this by adding all the binary expressions composed of only unions
+    or intersections for every possible combination of elements of cuts. The algorithm is explained in the paper.
 
-    unions, intersections = set(), set()
-    unions.add(tuple(cuts[-1]))
-    intersections.add(tuple(cuts[-1]))
+    All the hashing stuff is necessary because numpy arrays are not hashable.
+
+    # TODO: It does not scale up very well. We might need to rethink this
+
+    Parameters
+    ----------
+    cuts: ndarray
+        The original cuts that we need to make submodular
+
+    Returns
+    -------
+    new_cuts: ndarray
+        The submodular cuts
+    """
+
+    current_cut = cuts[-1]
+    hash_current_cut = hash(current_cut.tostring())
+    unions = {hash_current_cut: current_cut}
+    intersections = {hash_current_cut: current_cut}
 
     for current_cut in cuts[-2::-1]:
-        current_cut = tuple(current_cut)
-        current_unions = set()
-        current_intersections = set()
+        current_unions, current_intersections = {}, {}
 
-        for cut in unions:
-            cut = np.array(cut, dtype='bool')
-            current_unions.add(tuple(current_cut | cut))
-            current_intersections.add(tuple(current_cut & ~cut))
+        for cut in unions.values():
+            v = current_cut | cut
+            k = hash(v.tostring())
+            current_unions.setdefault(k, v)
 
-        for cut in intersections:
-            cut = np.array(cut, dtype='bool')
-            current_unions.add(tuple(current_cut | ~cut))
-            current_intersections.add(tuple(current_cut & cut))
+            v = current_cut & ~cut
+            k = hash(v.tostring())
+            current_intersections.setdefault(k, v)
 
-        unions |= current_unions
-        intersections |= current_intersections
+        for cut in intersections.values():
+            v = current_cut | ~cut
+            k = hash(v.tostring())
+            current_unions.setdefault(k, v)
 
-    cuts = unions | intersections
-    cuts = np.array(list(cuts), dtype='bool')
-    return cuts
+            v = current_cut & cut
+            k = hash(v.tostring())
+            current_intersections.setdefault(k, v)
+
+        unions.update(current_unions)
+        intersections.update(current_intersections)
+
+    unions.update(intersections)
+    new_cuts = np.array(list(unions.values()), dtype='bool')
+
+    return new_cuts
