4527493189aa3f20aa9d489c5f1e5249fd7ed40a
Diego Fioravanti <fioravanti.diego@gmail.com>
2020-03-03 07:55:51 +0100

    Change signature + Added more tests

--- src/oriented_cuts.py
+++ src/oriented_cuts.py
@@ -6,17 +6,17 @@ import numpy as np
 class OrientedCut:
     """
     This class represents an oriented cut as a couple of lists.
-    In cuts it is stored the index of the cut in S, meanwhile in orientation
+    In idx_cuts it is stored the index of the cut in S, meanwhile in orientation
     we store which orientation we are taking as a bool.
     """
-    def __init__(self, cuts, orientations):
+    def __init__(self, idx_cuts, orientations):
 
         self.current = -1
         self._oriented_cuts = {}
-        if not isinstance(cuts, (list, np.ndarray)):
-            self._oriented_cuts[cuts] = orientations
+        if not isinstance(idx_cuts, (list, np.ndarray)):
+            self._oriented_cuts[idx_cuts] = orientations
         else:
-            for c, o in zip(cuts, orientations):
+            for c, o in zip(idx_cuts, orientations):
                 if c not in self._oriented_cuts:
                     self._oriented_cuts[c] = o
                 else:
@@ -25,7 +25,7 @@ class OrientedCut:
         self.size = len(self._oriented_cuts)
 
     def __hash__(self):
-        return hash(str(self.get_cuts()) + str(self.get_orientations()))
+        return hash(str(self.get_idx_cuts()) + str(self.get_orientations()))
 
     def __iter__(self):
         return self
@@ -40,16 +40,19 @@ class OrientedCut:
         return cut, orientation
 
     def __eq__(self, other):
-        if self.get_cuts() == other.get_cuts() and self.get_orientations() == other.get_orientations():
+        if self.get_idx_cuts() == other.get_idx_cuts() and self.get_orientations() == other.get_orientations():
             return True
         else:
             return False
 
-    def get_cuts(self):
+    def get_idx_cuts(self):
         return list(self._oriented_cuts.keys())
 
     def get_orientations(self):
         return list(self._oriented_cuts.values())
+
+    def orientation_of(self, i):
+        return self._oriented_cuts.get(i)
 
     def add(self, cut, orientation):
         if cut in self._oriented_cuts:
--- test/test_oriented_cuts.py
+++ test/test_oriented_cuts.py
@@ -6,17 +6,17 @@ def test_oriented_cuts():
     # Creation
 
     cuts, orientations = 0, True
-    c = OrientedCut(cuts=cuts, orientations=orientations)
-    assert c.get_cuts() == [cuts] and c.get_orientations() == [orientations]
+    c = OrientedCut(idx_cuts=cuts, orientations=orientations)
+    assert c.get_idx_cuts() == [cuts] and c.get_orientations() == [orientations]
 
     cuts = [0, 1, 2, 3, 4]
     orientations = [True, False, True, True, False]
-    or_cuts = OrientedCut(cuts=cuts, orientations=orientations)
+    or_cuts = OrientedCut(idx_cuts=cuts, orientations=orientations)
     for i, (c, o) in enumerate(or_cuts):
         assert c == cuts[i] and o == orientations[i]
 
-    c1 = OrientedCut(cuts=0, orientations=True)
-    c2 = OrientedCut(cuts=0, orientations=False)
+    c1 = OrientedCut(idx_cuts=0, orientations=True)
+    c2 = OrientedCut(idx_cuts=0, orientations=False)
     assert c1 == c1
     assert c1 != c2
 
@@ -25,9 +25,14 @@ def test_oriented_cuts():
     cuts = [0, 1, 2, 3, 4]
     o1 = [True, False, True, True, False]
     o2 = [True, False, True, True, True]
-    c1 = OrientedCut(cuts=cuts, orientations=o1)
-    c2 = OrientedCut(cuts=cuts, orientations=o2)
+    c1 = OrientedCut(idx_cuts=cuts, orientations=o1)
+    c2 = OrientedCut(idx_cuts=cuts, orientations=o2)
     assert c1 != c2
+
+    # Get
+    assert not c1.orientation_of(4)
+    assert c2.orientation_of(4)
+    assert c2.orientation_of(6) is None
 
     # hashing
 
@@ -41,19 +46,21 @@ def test_oriented_cuts():
 
     cuts, cut = [0, 1, 2], 3
     orrs, orr = [True, False, True], True
-    c1 = OrientedCut(cuts=cuts, orientations=orrs)
-    c2 = OrientedCut(cuts=cuts, orientations=orrs)
+    c1 = OrientedCut(idx_cuts=cuts, orientations=orrs)
+    c2 = OrientedCut(idx_cuts=cuts, orientations=orrs)
     ref_cut = OrientedCut(cuts + [cut], orrs + [orr])
     c3, changed = c1.add(cut, orr)
     assert c1 == c2
     assert c1 != c3
     assert changed and c3 == ref_cut
     assert c3.size == c1.size + 1
+    assert c1.orientation_of(3) is None
+    assert c3.orientation_of(3)
 
     cuts, cut = [0, 1, 2], 1
     orrs, orr = [True, False, True], False
 
-    c = OrientedCut(cuts=cuts, orientations=orrs)
+    c = OrientedCut(idx_cuts=cuts, orientations=orrs)
     ref_cut = OrientedCut(cuts, orrs)
     c, changed = c.add(cut, orr)
     assert not changed and c == ref_cut
@@ -61,7 +68,7 @@ def test_oriented_cuts():
     cuts, cut = [0, 1, 2], 1
     orrs, orr = [True, False, True], True
 
-    c = OrientedCut(cuts=cuts, orientations=orrs)
+    c = OrientedCut(idx_cuts=cuts, orientations=orrs)
     ref_cut = OrientedCut(cuts, orrs)
     c, changed = c.add(cut, orr)
     assert not changed and c == ref_cut
