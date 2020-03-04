00f6ba24cfcffb1d3bb3eb029b353972d614fccd
Diego Fioravanti <fioravanti.diego@gmail.com>
2020-03-04 10:08:00 +0100

    Update gitingore

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python3 main.py --dat_path ./datasets/questionnaire/synthetic_s_200_f_3_m_3_t_34.0% --pre_name sub --alg_name basic --dat_type q_syn
+python3 main.py --dat_path ./datasets/questionnaire/synthetic_s_300_f_5_m_4_t_20.0% --pre_name sub --alg_name basic --dat_type q_syn
+# python3 main.py --dat_type iris --pre_name sub --alg_name basic
