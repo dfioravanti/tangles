c6fb09ebf0e7ec4ef1144f9f1e27f13d6eaa143b
Diego Fioravanti <fioravanti.diego@gmail.com>
2020-04-20 14:35:56 +0200

    Added printing of how the best tangles answer questions

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,17 @@ def dict_product(dicts):
      {'character': 'b', 'number': 2}]
     """
     return (dict(zip(dicts.keys(), x)) for x in itertools.product(*dicts.values()))
+
+
+def change_lower(interval, new_value):
+    if new_value > interval[0]:
+        return new_value, interval[1]
+
+    return interval
+
+
+def change_upper(interval, new_value):
+    if new_value < interval[1]:
+        return interval[0], new_value
+
+    return interval
