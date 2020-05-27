Fixed bugs in the computation of p

src/execution.py
@@ -346,12 +346,13 @@ def centers_in_range_answers(cs, range_answers):
 
 def compute_soft_predictions(contracted_tree, cuts, orders, verbose):
 
-    costs = np.exp(-normalize(orders))
+    cost_function = lambda array: np.exp(-normalize(array))
 
     compute_soft_predictions_children(node=contracted_tree.root,
-                                  cuts=cuts,
-                                  costs=costs,
-                                  verbose=verbose)
+                                      cuts=cuts,
+                                      orders=orders,
+                                      cost_function=cost_function,
+                                      verbose=verbose)
 
     
 def compute_and_save_evaluation(ys, ys_predicted, hyperparameters, id_run, path):

src/plotting.py
@@ -11,7 +11,7 @@ from sklearn.manifold import TSNE
 from sklearn.preprocessing import minmax_scale
 
 from src.config import NAN
-from src.utils import get_points_to_plot, normalize
+from src.utils import get_points_to_plot
 
 
 # Standard colors for uniform plots
@@ -20,8 +20,6 @@ COLOR_SILVER_RGB = (192 / 255, 192 / 255, 192 / 255) + (0.2,)
 COLOR_INDIGO_RGB = (55 / 255, 0 / 255, 175 / 255) + (0.5,)
 COLOR_CARNATION_RGB = np.array((247 / 255, 96 / 255, 114 / 255, 1)).reshape((1, -1))
 CMAP = plt.cm.get_cmap('Blues')
-
-# TODO: Fix the comments in this file
 
 def get_nb_points(data):
 
@@ -51,10 +49,10 @@ def get_next_id(current_id, direction):
     else:
         return current_id + 2 ** level + 2
 
-def plot_dataset(data, colors, ax=None, cmap=None, add_colorbar=True, pos=None):
+def plot_dataset(data, colors, ax=None, eq_cuts=None, cmap=None, add_colorbar=True, pos=None):
 
     if data['xs'] is not None:
-        ax = plot_dataset_metric(data['xs'], data['cs'], colors, ax, cmap, add_colorbar)
+        ax = plot_dataset_metric(data['xs'], data['cs'], colors, eq_cuts, ax, cmap, add_colorbar)
     elif data['G'] is not None:
         ax, pos = plot_dataset_graph(data['G'], data['ys'], colors, ax, cmap, add_colorbar, pos)
 
@@ -82,7 +80,7 @@ def plot_dataset_graph(G, ys, colors, ax, cmap, add_colorbar, pos):
     return ax, pos
 
 
-def plot_dataset_metric(xs, cs, colors, ax, cmap, add_colorbar):
+def plot_dataset_metric(xs, cs, colors, eq_cuts, ax, cmap, add_colorbar):
 
     plt.style.use('ggplot')
     plt.ioff()
@@ -94,6 +92,12 @@ def plot_dataset_metric(xs, cs, colors, ax, cmap, add_colorbar):
     xs_embedded, cs_embedded = get_points_to_plot(xs, cs)
 
     sc = ax.scatter(xs_embedded[:, 0], xs_embedded[:, 1], color=colors, vmin=0, vmax=1, edgecolor='black')
+
+    if eq_cuts is not None:
+        for eq in eq_cuts:
+            x, y =  get_lines(xs, eq)
+            ax.plot(x, y ,'k--')
+    
     if add_colorbar:
         ax = add_colorbar_to_ax(ax, cmap)
 
@@ -113,7 +117,7 @@ def labels_to_colors(ys, cmap):
     return colors        
 
 
-def plot_soft_predictions(data, contracted_tree, id_node=0, path=None):
+def plot_soft_predictions(data, contracted_tree, eq_cuts=None, id_node=0, path=None):
 
     plt.style.use('ggplot')
     plt.ioff()
@@ -128,33 +132,36 @@ def plot_soft_predictions(data, contracted_tree, id_node=0, path=None):
     if data['ys'] is not None:
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 10))
         colors = labels_to_colors(data['ys'], cmap=cmap_groundtruth)
-        ax, pos = plot_dataset(data, colors, ax=ax, add_colorbar=False)
+        ax, pos = plot_dataset(data, colors, eq_cuts=eq_cuts, ax=ax, add_colorbar=False)
 
         fig.savefig(output_path / f"groundtruth.svg")
         plt.close(fig)
 
-    plot_soft_prediction_node(data, contracted_tree.root, id_node=0, cmap=cmap_heatmap, path=path, pos=pos)
+    plot_soft_prediction_node(data, contracted_tree.root, eq_cuts=eq_cuts, id_node=0, cmap=cmap_heatmap, path=path, pos=pos)
     
 
-def plot_soft_prediction_node(data, node, id_node, cmap, path, pos):
+def plot_soft_prediction_node(data, node, eq_cuts, id_node, cmap, path, pos):
 
-    if node.p is None:
-        nb_points = get_nb_points(data)
-        colors = cmap(np.ones(nb_points))
-    else:
-        colors = cmap(node.p)
+    colors = cmap(node.p)
     
+    if eq_cuts is not None:
+        if len(node.characterizing_cuts) != 0:
+            id_characterizing_cuts = list(node.characterizing_cuts.keys())
+            eq_characterizing_cuts = eq_cuts[id_characterizing_cuts]
+        else:
+            eq_characterizing_cuts = []
+
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20, 10))
-    plot_dataset(data, colors, ax=ax, cmap=cmap, pos=pos)
+    plot_dataset(data, colors, eq_cuts=eq_characterizing_cuts, ax=ax, cmap=cmap, pos=pos)
     fig.savefig(path / f"node_nb_{id_node:02d}.svg")
     plt.close(fig)
 
     if node.left_child is not None:
         id_left = get_next_id(id_node, 'left')
-        plot_soft_prediction_node(data, node.left_child, id_left, cmap, path, pos=pos)
+        plot_soft_prediction_node(data, node.left_child, eq_cuts, id_left, cmap, path, pos=pos)
     if node.right_child is not None:
         id_right = get_next_id(id_node, 'right')
-        plot_soft_prediction_node(data, node.right_child, id_right, cmap, path, pos=pos)
+        plot_soft_prediction_node(data, node.right_child, eq_cuts, id_right, cmap, path, pos=pos)
 
 
 def plot_hard_predictions(data, ys_predicted, path=None):
@@ -213,7 +220,7 @@ def plot_cuts(data, cuts, orders, nb_cuts_to_plot, path):
     pos = None
         
     for i in np.arange(nb_cuts_to_plot):
-        eq = eq_cuts[i] if eq_cuts is not None else None
+        eq = [eq_cuts[i]] if eq_cuts is not None else None
             
         fig, pos = plot_cut(data, cut=values_cuts[i], order=orders[i], eq=eq, pos=pos)
         if path is not None:
@@ -252,11 +259,8 @@ def plot_cut(data, cut, order, eq, pos):
     if data['ys'] is not None:
         fig, (ax_true, ax_cut) = plt.subplots(nrows=1, ncols=2, figsize=(20, 10))
         colors_true = labels_to_colors(data['ys'], cmap=cmap_groundtruth)
-        ax_true, pos = plot_dataset(data, colors_true, ax=ax_true, add_colorbar=False, pos=pos)
+        ax_true, pos = plot_dataset(data, colors_true, eq_cuts=eq, ax=ax_true, add_colorbar=False, pos=pos)
         ax_true.set_title('Groundtruth')
-        if eq is not None:
-            x, y =  get_lines(data['xs'], eq)
-            ax_true.plot(x, y ,'k--')
         
     else:
         fig, ax_cut = plt.subplots(nrows=1, ncols=1, figsize=(20, 10))

src/tree_tangles.py
@@ -72,8 +72,8 @@ class ContractedTangleNode(TangleNode):
         else:
             padding = ' '
             string_cuts = [f'{k} -> {v}' for k, v in self.characterizing_cuts.items()]
-            string = f'{padding*height}{string_cuts}'
-            
+            string = f'{padding*height}- {string_cuts}'
+                    
         if self.left_child is not None:
             string += '\n'
             string += self.left_child.__str__(height=height+1)
@@ -381,50 +381,85 @@ def contract_tree(tree):
     return contracted_tree
 
 
-def compute_soft_predictions_node(node, characterizing_cuts, cuts, costs, verbose):
+def compute_soft_predictions_node(node, characterizing_cuts, cuts, orders, cost_function, verbose):
 
-    if not node.last_cut_added_id in characterizing_cuts:
-        characterizing_cuts[node.last_cut_added_id] = Orientation(node.last_cut_added_orientation)
-    
-    idx_characterizing_cuts = list(characterizing_cuts.keys())
-    orientation_characterizing_cuts = [True if o.direction == 'left' else False for o in characterizing_cuts.values()]
+    if node.left_child is None and node.right_child is None:
+        if not node.last_cut_added_id in characterizing_cuts:
+            characterizing_cuts[node.last_cut_added_id] = Orientation(node.last_cut_added_orientation)
 
-    p = np.sum((cuts[idx_characterizing_cuts, :].T == orientation_characterizing_cuts) * costs[idx_characterizing_cuts], axis=1)
-    p = p * node.parent.p
+    idx_characterizing_cuts, orientation_characterizing_cuts = [], []
+    for i, o in characterizing_cuts.items():
+        if o.direction == 'left':
+            idx_characterizing_cuts.append(i)
+            orientation_characterizing_cuts.append(True)
+        elif o.direction == 'right':
+            idx_characterizing_cuts.append(i)
+            orientation_characterizing_cuts.append(False)
+
+    costs = cost_function(orders[idx_characterizing_cuts])
+
+    p = np.sum((cuts[idx_characterizing_cuts, :].T == orientation_characterizing_cuts) * costs, axis=1)
 
     return p
 
 
-def compute_soft_predictions_children(node, cuts, costs, verbose):
+def compute_soft_predictions_children(node, cuts, orders, cost_function, verbose):
 
     _, nb_points = cuts.shape
+    
     if node.parent is None:
         node.p = np.ones(nb_points)  
-
-    if node.left_child is not None and node.right_child is not None:
-
+    
+    if node.left_child is not None and node.right_child is None:
+        node.left_child.p = np.ones(nb_points)
+        compute_soft_predictions_children(node=node.left_child, 
+                                          cuts=cuts,
+                                          orders=orders, 
+                                          cost_function=cost_function,
+                                          verbose=verbose)    
+    elif node.left_child is None and node.right_child is not None:
+        node.right_child.p = np.ones(nb_points)
+        compute_soft_predictions_children(node=node.right_child, 
+                                          cuts=cuts,
+                                          orders=orders, 
+                                          cost_function=cost_function,
+                                          verbose=verbose)
+    elif node.left_child is not None and node.right_child is not None:
         p_left = np.zeros(nb_points)
         p_right = np.zeros(nb_points)
 
-        unnormalized_p_left = compute_soft_predictions_node(node=node.left_child, characterizing_cuts=node.characterizing_cuts_left,
-                                      cuts=cuts, costs=costs, verbose=verbose)
-        unnormalized_p_right = compute_soft_predictions_node(node=node.right_child, characterizing_cuts=node.characterizing_cuts_right, 
-                                      cuts=cuts, costs=costs, verbose=verbose)
+        unnormalized_p_left = compute_soft_predictions_node(node=node.left_child, 
+                                                            characterizing_cuts=node.characterizing_cuts_left,
+                                                            cuts=cuts, 
+                                                            orders=orders, 
+                                                            cost_function=cost_function,
+                                                            verbose=verbose)
+        unnormalized_p_right = compute_soft_predictions_node(node=node.right_child, 
+                                                            characterizing_cuts=node.characterizing_cuts_right,
+                                                            cuts=cuts, 
+                                                            orders=orders, 
+                                                            cost_function=cost_function,
+                                                            verbose=verbose)
 
         total_p = unnormalized_p_left + unnormalized_p_right
-        idx_not_zero = ~np.isclose(total_p, 0)
+            
+        p_left = unnormalized_p_left / total_p
+        p_right = unnormalized_p_right / total_p
 
-        p_left[idx_not_zero] = unnormalized_p_left[idx_not_zero] / total_p[idx_not_zero]
-        p_right[idx_not_zero] = unnormalized_p_right[idx_not_zero] / total_p[idx_not_zero]
+        node.left_child.p = p_left * node.p
+        node.right_child.p = p_right * node.p
 
-        node.left_child.p = p_left
-        node.right_child.p = p_right
+        compute_soft_predictions_children(node=node.left_child, 
+                                        cuts=cuts,
+                                        orders=orders, 
+                                        cost_function=cost_function,
+                                        verbose=verbose)   
 
-    if node.left_child is not None:
-        compute_soft_predictions_children(node=node.left_child, cuts=cuts, costs=costs, verbose=verbose)
-    if node.right_child is not None:
-        compute_soft_predictions_children(node=node.right_child, cuts=cuts, costs=costs, verbose=verbose)
-
+        compute_soft_predictions_children(node=node.right_child, 
+                                        cuts=cuts,
+                                        orders=orders, 
+                                        cost_function=cost_function,
+                                        verbose=verbose)
 
 def compute_hard_predictions_node(node, idx_points, max_tangles):
 