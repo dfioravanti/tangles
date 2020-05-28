51e59b8a5ae10c954834a35969b8e82f03b07c9b
Diego Fioravanti <fioravanti.diego@gmail.com>
2020-05-28 07:34:53 +0200

    New experiments + benchmarks to run

--- src/datasets/cancer.py
+++ src/datasets/cancer.py
@@ -7,14 +7,13 @@ from sklearn.datasets import load_breast_cancer
 def load_CANCER(nb_bins):
 
     data = load_breast_cancer()
-
     feature_names, xs, ys = data.feature_names, data.data, data.target
-    df = pd.DataFrame(data=xs, columns=feature_names)
+    
+    if nb_bins > 0:
+        df = pd.DataFrame(data=xs, columns=feature_names)
+        df_bined = pd.DataFrame()
+        for column in df.columns:
+            df_bined[column] = pd.qcut(df[column], q=nb_bins, labels=False) + 1
 
-    df_bined = pd.DataFrame()
-    for column in df.columns:
-
-        df_bined[column] = pd.qcut(df[column], q=nb_bins, labels=False) + 1
-
-    xs = df_bined.values
+        xs = df_bined.values
     return xs, ys
