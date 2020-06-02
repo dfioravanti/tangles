a463bb79fd0bb6c6f61600631bc8774e62fd2153
Diego Fioravanti <fioravanti.diego@gmail.com>
2020-06-02 07:48:11 +0200

    Clean up + added readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# tangles
+# Tangles
+
+This is the code to run the program associated with the paper: "Tangles: from weak to strong clustering".
+
+The program can be used in two ways:
+
+1. Set the paramenters in `settings.yml` and then run `python main.py`. This is the prefered way the program is intended to be used.
+2. Pass a set of parameter to `python main.py` that overrides the parameters in `settings.yml`. Paramenters that are not set default to the one specified in `settings.yml`. This is how we can run bulk experiments in parallel.
+
+Example: `python main.py --id 1591047201 -t mindsets -p features -a 10 -s 10 -o 10 --mind_sizes 40 40 --mind_questions 40 --mind_useless 40 --mind_noise 0.1 --no_plots
+`
+
+--------
+
+1. **Specification of dependencies**:
+   We provide a `requirements.txt` file in the zip.
+
+2. **Training / Evaluation code**:
+   We provide such code in the `src` folder.
+
+3. **Pre-trained models**:
+   Not applicable, our model trains in less than a minute.
+
+4. **Table of results with paramentes**: The majority of our experiments are comparisons among different sets of paramenter in order to investigate the properties of the new algorithm that we are introducing.  Each experiment was created using the `make_experiment.py` file.  Each experiment was averaged over 10 runs with seeds 10-100. By using `make_experiments.py` you can recreate the list of parameters for each experiment. The parameters are given in the paper near every figure.
--- make_experiments.py
+++ make_experiments.py
@@ -2,9 +2,7 @@ import datetime
 
 import numpy as np
 
-from src.config import DATASET_SBM, DATASET_MINDSETS, PREPROCESSING_KARNIG_LIN, \
-                       PREPROCESSING_FID_MAT, PREPROCESSING_USE_FEATURES, \
-                       DATASET_QUESTIONNAIRE, PREPROCESSING_BINARIZED_LIKERT, PREPROCESSING_USE_FEATURES
+from src.config import DATASET_SBM, DATASET_MINDSETS, PREPROCESSING_FID_MAT, PREPROCESSING_USE_FEATURES
 from src.utils import dict_product
 
 ts = int(datetime.datetime.now().timestamp())
@@ -16,35 +14,24 @@ parameters['--id'] = ts
 parameters['-t'] = DATASET_MINDSETS
 
 multi_parameters['-s'] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-#multi_parameters['-s'] = [42]
 if parameters['-t'] == DATASET_SBM:
 
-    """ parameters['-p'] = PREPROCESSING_FID_MAT
-    multi_parameters['-o'] = [80]
-    multi_parameters['--nb_cuts'] = [20]
-    multi_parameters['--lb_f'] = [0.2, 0.3, 0.4]
-    multi_parameters['-a'] = [50]
-    multi_parameters['--sbm_bs'] = [[100, 100]]
-    multi_parameters['--sbm_p'] = [0.05, 0.09, 0.14, 0.18, 0.23, 0.28, 0.32, 0.37, 0.41, 0.46, 0.51, 0.55, 0.6, 0.64, 0.69, 0.74, 0.78, 0.83, 0.87, 0.92]
-    multi_parameters['--sbm_q'] = [0.05, 0.09, 0.14, 0.18, 0.23, 0.28, 0.32, 0.37, 0.41, 0.46, 0.51, 0.55, 0.6, 0.64, 0.69, 0.74, 0.78, 0.83, 0.87, 0.92]
- """
- 
     sizes = np.linspace(5, 60, 10).astype(int)
- 
+
     parameters['-p'] = PREPROCESSING_FID_MAT
     multi_parameters['-o'] = np.arange(1, 11) * 10
     multi_parameters['-a'] = np.linspace(10, 60, 11).astype(int)
     multi_parameters['--nb_cuts'] = [20]
     multi_parameters['--lb_f'] = [0.2]
-    multi_parameters['--sbm_bs'] = [[100, 100] ]
+    multi_parameters['--sbm_bs'] = [[100, 100]]
     multi_parameters['--sbm_p'] = [0.3]
     multi_parameters['--sbm_q'] = [0.1]
-    
- 
+
+
 elif parameters['-t'] == DATASET_MINDSETS:
-        
+
     sizes = np.linspace(5, 60, 10).astype(int)
-        
+
     parameters['-p'] = PREPROCESSING_USE_FEATURES
     multi_parameters['-o'] = np.arange(1, 11) * 10
     multi_parameters['-a'] = np.linspace(10, 100, 9).astype(int)
@@ -52,15 +39,10 @@ elif parameters['-t'] == DATASET_MINDSETS:
     multi_parameters['--mind_questions'] = [10, 20, 40]
     multi_parameters['--mind_useless'] = [10, 20, 40]
     multi_parameters['--mind_noise'] = [0.1, 0.2]
-    
+
 with open('parameters.txt', 'w') as f:
     for current_parameters in dict_product(multi_parameters):
-        
-        if parameters['-t'] == DATASET_MINDSETS:
-            parameters['-a'] = current_parameters['--mind_sizes'][1] // 3
-        if parameters['-t'] == DATASET_SBM:
-            parameters['-a'] = current_parameters['--sbm_bs'][1] // 2
-                    
+
         p = {**parameters, **current_parameters}
 
         line = []
